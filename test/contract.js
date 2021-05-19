751f0bd33f757093b3d023f8cc72b36a1d5ac0b0
wnstj2007 <wnstj2096@gmail.com>
2021-05-19 17:35:31 +0900

    modify test code

--- test/contract.js
+++ test/contract.js
@@ -4,7 +4,9 @@ contract("Storage" , () => {
     it("Storage Testing" , async () => {
        const storage = await Storage.deployed() ;
        await storage.addFood(1, "Food", "Korean") ;
+       await storage.addFood(1, "Food2", "Korean") ;
        const result = await storage.getFood(1) ;
        assert(result[0].name === "Food") ;
+       console.log(result);
     });
 });
